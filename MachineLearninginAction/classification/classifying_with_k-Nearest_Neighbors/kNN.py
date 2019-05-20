309f423d928938a7c138ca85a3e0b13fa2148cb3
vodaka <vodaka@126.com>
2019-05-20 17:25:19 +0800

    20190520

--- MachineLearninginAction/classification/classifying_with_k-Nearest_Neighbors/kNN.py
+++ MachineLearninginAction/classification/classifying_with_k-Nearest_Neighbors/kNN.py
@@ -2,4 +2,22 @@ import numpy as np
 import operator
 
 def createDataSet():
-    group = np.array([])
+    group = np.array([
+        [1.0, 1.1],
+        [1.0, 1.0],
+        [0, 0],
+        [0, 0.1]
+    ])
+    labels = ['A','A','B','B']
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.title(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+
